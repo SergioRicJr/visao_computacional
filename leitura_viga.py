587a1e7e93a9eabd1638950501f0cebac6d39b47
Sergio <sergioricjr7@gmil.com>
2022-12-29 23:39:18 -0300

    projeto em estado de uso final

--- leitura_viga.py
+++ leitura_viga.py
@@ -14,20 +14,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 #usar '_' para dividir nomes do pavimento -- ex terreo_interno_2
 
 
-#EXPERENCIA DO USUARIO --- 
 
-# escolha = int(input("""Digite:
-#                         1 - Verificar se a imagem já foi lida anteriormente
-#                         2 - Ler uma única imagem
-#                         3 - Ler todas as imagens na pasta
-#                         """))
-
-# if escolha == 1:
-#   pass
-# if escolha == 2:
-#   pass
-# if escolha == 3:
-#   pass
 
 class planta_vigapilar:
   def __init__(self):
@@ -40,6 +27,8 @@ class planta_vigapilar:
         'padrao_nao_pastas': '^[\w\W]+\.[A-Za-z]+$',
         'padrao_nome_excel_generico': '^vigas_[A-Za-zÀ-Úà-ú_\s]+[0-9]?_[0-9]{3}_[A-Za-zÀ-Úà-ú]+\.xlsx$',
         'padrao_n_planta': '(Planta|planta|PLANTA)-(vp|VP|Vp)-[\w\W_]{3,40}[0-9]?-[\d]{3}-[\w\W]+\.(jpg|png|pdf)',
+        'padrao_n_planta_cnum': '(Planta|planta|PLANTA)-(vp|VP|Vp)-[A-Za-z_]{3,40}[0-9]{1}-[\d]{3}-[\w\W]+\.(jpg|png|pdf)',
+        'padrao_n_planta_snum': '(Planta|planta|PLANTA)-(vp|VP|Vp)-[a-zA-Z_]{3,40}-[\d]{3}-[\w\W]+\.(jpg|png|pdf)',
         'p_nome_1': '^(V|v)[0-9]{1}',
         'p_nome_2': '^(V|v)[0-9]{2}',
         'p_vep':'^[0-9]{2}(x|X)[0-9]{2}$',
@@ -53,7 +42,8 @@ class planta_vigapilar:
     self.lista_excel_pronto = []
     self.diretorio = os.getcwd()
     self.lista_arquivos = os.listdir(os.getcwd())
-  
+    self.imagens_lidas = []
+
   def iniciar_processo_individual(self):  #algo de errado aqui, o not do criar pasta esta invertido
     lista_arquivos = os.listdir(os.getcwd())
     self.listar_arquivos_prontos(self.diretorio)
@@ -80,21 +70,26 @@ class planta_vigapilar:
       self.dividir_tam_viga()
       self.criar_df()
       self.ordem_df() 
-      if f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}" not in lista_arquivos: #TRANSFORMAR ISSO EM FUNCAO
+      if f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}" not in self.lista_arquivos:
         os.mkdir(f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}')
-      os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}-{self.n_obra}-{self.nome_cliente}.jpg') 
-      with pd.ExcelWriter(f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}\\vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.jpg", engine='openpyxl',mode='a', if_sheet_exists='overlay') as writer: 
+      if re.match(self.exre['padrao_n_planta_snum'], self.caminho_img):
+        os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}-{self.n_obra}-{self.nome_cliente}.jpg') 
+      if re.match(self.exre['padrao_n_planta_cnum'], self.caminho_img):
+        os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}{self.numero_foto_obra}-{self.n_obra}-{self.nome_cliente}.jpg')
+      with pd.ExcelWriter(f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}\\vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx", engine='openpyxl',mode='a', if_sheet_exists='overlay') as writer: 
         self.df.to_excel(writer, sheet_name='Sheet1', startrow=writer.sheets["Sheet1"].max_row, index=False, header=False)
   
   def ler_plantas_automaticamente(self):
-    self.listar_arquivos_prontos(os.getcwd())
     self.lista_arquivos = os.listdir(os.getcwd())
     for arquivo in self.lista_arquivos:
+        self.lista_arquivos = os.listdir(os.getcwd())
+        lista_arquivos = os.listdir(os.getcwd())
+        self.listar_arquivos_prontos(os.getcwd())
         self.vigas_tam = []
         self.vigas_nome = []
         self.v_x = []
         self.v_y = []
-        if re.match(self.exre['padrao_nome_planta_2'], arquivo):
+        if re.match(self.exre['padrao_n_planta'], arquivo):
             a =  arquivo
             b = a.split('.')
             info_nome = b[0].split("-")
@@ -102,6 +97,9 @@ class planta_vigapilar:
             pavimento = info_nome[2]
             n_obra = info_nome[3]
             nome_cliente = info_nome[4]
+            if re.match('[\w\W_]{3,40}[0-9]{1}', info_nome[2]):
+              numero_foto_obra = pavimento[-1]
+              pavimento = pavimento[:-1]
             if f'vigas_{pavimento}_{n_obra}_{nome_cliente}.xlsx' not in self.lista_excel_pronto:
                 self.caminho_img = arquivo
                 self.img = cv2.imread(self.caminho_img)
@@ -116,19 +114,33 @@ class planta_vigapilar:
                 self.ordem_df()
                 self.exportar_df()
 
-            else: 
-              print('O arquivo já existe')
-              if f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}" not in self.lista_arquivos:
-                os.mkdir(f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}')
-              os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}-{self.n_obra}-{self.nome_cliente}') 
-              with pd.ExcelWriter(f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}\\vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.jpg", engine='openpyxl',mode='a', if_sheet_exists='overlay') as writer: 
+            else:
+              print('O arquivo excel já existe')
+              self.caminho_img = arquivo
+              self.img = cv2.imread(self.caminho_img)
+              self.ler_imagem(self.img)
+              self.add_info_list(self.resultado)
+              self.muda_lado_90()
+              self.ler_imagem(self.img_virada)
+              self.add_info_list(self.resultado)
+              self.dividir_tam_viga()
+              self.criar_df()
+              self.ordem_df()
+              if f"VIGAS_E_PILARES_{n_obra}_{nome_cliente.upper()}" not in lista_arquivos:
+                os.mkdir(f'VIGAS_E_PILARES_{n_obra}_{nome_cliente.upper()}')
+              if re.match(self.exre['padrao_n_planta_snum'], self.caminho_img):
+                os.rename(self.caminho_img, f'VIGAS_E_PILARES_{n_obra}_{nome_cliente.upper()}/planta-vp-{pavimento}-{n_obra}-{nome_cliente}.jpg') 
+              if re.match(self.exre['padrao_n_planta_cnum'], self.caminho_img):
+                os.rename(self.caminho_img, f'VIGAS_E_PILARES_{n_obra}_{nome_cliente.upper()}/planta-vp-{pavimento}{numero_foto_obra}-{n_obra}-{nome_cliente}.jpg')
+              with pd.ExcelWriter(f"VIGAS_E_PILARES_{n_obra}_{nome_cliente.upper()}\\vigas_{pavimento}_{n_obra}_{nome_cliente}.xlsx", engine='openpyxl',mode='a', if_sheet_exists='overlay') as writer: 
                 self.df.to_excel(writer, sheet_name='Sheet1', startrow=writer.sheets["Sheet1"].max_row, index=False, header=False)
 
   def carregar_imagem(self):
     self.caminho_img = input('Digite o nome da imagem: ')
     self.img = cv2.imread(self.caminho_img)
-    self.img = cv2.cvtColor(self.img, cv2.COLOR_BGRA2GRAY)
-  
+    self.img = cv2.cvtColor(self.img, cv2.COLOR_BGRA2GRAY) #binarizar imagem depois para ver se melhora performance
+    _, self.img = cv2.threshold(self.img, 90, 255, cv2.THRESH_BINARY)
+
   def informacoes_nome(self):
     a = re.finditer(self.exre['padrao_n_planta'], self.caminho_img)
     for i in a:
@@ -194,20 +206,65 @@ class planta_vigapilar:
     self.df.reset_index(drop=True, inplace=True)
 
   def exportar_df(self):
-    lista_arquivos = os.listdir(os.getcwd())
+    self.lista_arquivos = os.listdir(os.getcwd())
     self.df.to_excel(f"vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx", index=False)
-    if f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}" not in lista_arquivos:
+    if f"VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}" not in self.lista_arquivos:
       os.mkdir(f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}')
-    os.rename(f'vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx', f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx')           
-    os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}-{self.n_obra}-{self.nome_cliente}.jpg') 
+    if re.match(self.exre['padrao_n_planta_snum'], self.caminho_img):
+      os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}-{self.n_obra}-{self.nome_cliente}.jpg') 
+    if re.match(self.exre['padrao_n_planta_cnum'], self.caminho_img):
+      os.rename(self.caminho_img, f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/planta-vp-{self.pavimento}{self.numero_foto_obra}-{self.n_obra}-{self.nome_cliente}.jpg')
+    os.rename(f'vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx', f'VIGAS_E_PILARES_{self.n_obra}_{self.nome_cliente.upper()}/vigas_{self.pavimento}_{self.n_obra}_{self.nome_cliente}.xlsx')
 
   def listar_arquivos_prontos(self, pasta):
     self.lista_arquivos = os.listdir(pasta)
     for arquivo in self.lista_arquivos:
         if not re.match(self.exre['padrao_nao_pastas'], arquivo) and arquivo != '.git':
-            self.listar_arquivos_prontos(arquivo)
+          self.listar_arquivos_prontos(arquivo)
         elif re.match(self.exre['padrao_nome_excel_generico'], arquivo):
-            self.lista_excel_pronto.append(arquivo)
+          self.lista_excel_pronto.append(arquivo)
+        if re.match(self.exre['padrao_n_planta'], arquivo):
+          self.imagens_lidas.append(arquivo)
 
 planta = planta_vigapilar()
-planta.ler_plantas_automaticamente()
+
+#EXPERENCIA DO USUARIO --- 
+
+escolha = int(input("""
+------------------------------------------------------ 
+Digite:
+1 - Para verificar se a imagem já foi lida anteriormente
+2 - Para Ler uma única imagem
+3 - Para Ler todas as imagens na pasta
+4 - Para sair do programa
+------------------------------------------------------
+"""))
+print('------------------------------------------------------')
+while True:  
+  imagens_lidas = []
+  imagens_prontas = []
+  planta.imagens_lidas = []
+  if escolha == 1:
+    planta.listar_arquivos_prontos(os.getcwd())
+    for i in planta.imagens_lidas:
+      imagens_lidas.append(i.lower())
+    for i in imagens_lidas: 
+      if imagens_lidas.count(i) > 1:
+        imagens_prontas.append(i)
+    print(f'Os arquivos já feitos são: {str(set(imagens_prontas)).strip("{}").replace(","," e")}')
+  elif escolha == 2:
+    planta.iniciar_processo_individual()
+  elif escolha == 3:
+    planta.ler_plantas_automaticamente()
+  elif escolha == 4:
+    print('Obrigado por usar o programa\n------------------------------------------------------')
+    break
+  escolha = int(input("""------------------------------------------------------ 
+Digite:
+1 - Para verificar se a imagem já foi lida anteriormente
+2 - Para Ler uma única imagem
+3 - Para Ler todas as imagens na pasta
+4 - Para sair do programa
+------------------------------------------------------
+"""))
+  print('------------------------------------------------------')
